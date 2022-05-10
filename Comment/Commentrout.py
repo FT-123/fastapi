6192e1c458acc4c11f18fc8e466c28df00e11acd
gidra <gidraiver@gmail.com>
2022-05-10 17:12:32 +0300

    New1

--- Comment/Commentrout.py
+++ Comment/Commentrout.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from .Commentrep import create_comment, verify_comment, get_comment, delete_comment
+from .Commentrep import CommentRepository
 from Photo.Photorep import PhotoRepository
 from .Commentschem import CommentBase, CommentList
 from dependencies import get_db
@@ -13,21 +13,22 @@ router = APIRouter(prefix="/Comment", tags=["comments"])
 
 @router.post("/api/photos/{photo_id}/comment", response_model=CommentList)
 def create_comments(
-        comment: CommentBase, photo_id: int, db: Session = Depends(get_db),
-        current_user: model.User = Depends(get_current_user)):
+        comment: CommentBase, photo_id: int, current_user: model.User = Depends(get_current_user),
+        comments: CommentRepository = Depends(), photos: PhotoRepository = Depends()):
     user_name = current_user.name
-    photo_have = PhotoRepository.get_post(db=db, id=photo_id)
+    photo_have = photos.get_post(id=photo_id)
     if not photo_have:
         raise HTTPException(status_code=404, detail="Photo not found")
-    return create_comment(db=db, photo_id=photo_id, name=user_name, comment=comment)
+    return comments.create_comment(photo_id=photo_id, name=user_name, comment=comment)
 
 
 @router.delete("/api/photos/comment/[COMMENT_ID]/")
-def photo_delete(comment_id: int, db: Session = Depends(get_db), current_user: model.User = Depends(get_current_user)):
+def photo_delete(comment_id: int, comments: CommentRepository = Depends(),
+                 current_user: model.User = Depends(get_current_user)):
     user_name = current_user.name
-    post = get_comment(db=db, id=comment_id)
-    stmt = verify_comment(db=db, id=comment_id, username=user_name)
+    post = comments.get_comment(id=comment_id)
+    stmt = comments.verify_comment(id=comment_id, username=user_name)
     if not stmt == post:
         raise HTTPException(status_code=404, detail="unauthorized")
-    dele = delete_comment(db=db, id=comment_id)
+    dele = comments.delete_comment(id=comment_id)
     return dele
